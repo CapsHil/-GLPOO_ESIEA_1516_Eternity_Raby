24303128766d2e38e4a9f65cd248cb09f0b35acd
Cous-Cous <sami.kartit@gmail.com>
2016-04-22 13:49:08 +0200

    Create project

---
+++ eternity/src/main/java/org/esiea/glpoo/eternity/App.java
@@ -0,0 +1,13 @@
+package org.esiea.glpoo.eternity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
